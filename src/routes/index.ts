7b8fc9bdfd452853d197f1a52c5606312625843a
Renato W Schlogel <renato.s@outlook.com>
2020-05-01 11:52:02 -0300

    sinc

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,10 @@ import { Router } from 'express';
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  return response.json({mensagem: "Parabéns :D, aplicação respondendo com sucesso!"});
+});
+
 routes.post('/users', (request, response) => {
   const { name, email } = request.body;
 
